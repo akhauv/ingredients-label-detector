2d4009d4a0c51df47f5df3f2ce99a56520275e2b
Amber <akkhauv@gmail.com>
2024-06-29 01:20:14 -0700

    added merging boxes to dext detection for vertically close text lines to detect paragraphs

--- scripts/text_detection.py
+++ scripts/text_detection.py
@@ -33,17 +33,97 @@ def resize_img(img, scale, max_scale=None):
         f = float(max_scale) / max(img.shape[0], img.shape[1])
     return cv2.resize(img, None, None, fx=f, fy=f, interpolation=cv2.INTER_LINEAR), f
 
+def filter_boxes(boxes):
+    # Initialize a boolean mask with true values
+    mask = np.ones(len(boxes), dtype=bool)
+    
+    # Set mask to False for boxes to be filtered out
+    for i, box in enumerate(boxes):
+        if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3] - box[0]) < 5:
+            mask[i] = False
+    
+    # Apply the mask to filter the boxes
+    filtered_boxes = boxes[mask]
+    return filtered_boxes
+
+def get_coords(box):
+    min_x = min(box[0], box[2], box[4], box[6])
+    min_y = min(box[1], box[3], box[5], box[7])
+    max_x = max(box[0], box[2], box[4], box[6])
+    max_y = max(box[1], box[3], box[5], box[7])
+    return min_x, min_y, max_x, max_y
+
+def has_overlap(box_one, box_two):
+    # vertical uncertainty is permissible becasue of thin lines
+    uncertainty = 8
+
+    one_x_min, one_y_min, one_x_max, one_y_max = get_coords(box_one)
+    two_x_min, two_y_min, two_x_max, two_y_max = get_coords(box_two)
+
+    # check to see if they are overlapping
+    if (one_x_max < two_x_min) or (two_x_max < one_x_min):
+        return False
+    if (one_y_max + uncertainty < two_y_min) or (two_y_max + uncertainty < one_y_min):
+        return False
+
+    return True
+
+def merge_boxes(box_one, box_two):
+    one_x_min, one_y_min, one_x_max, one_y_max = get_coords(box_one)
+    two_x_min, two_y_min, two_x_max, two_y_max = get_coords(box_two)
+
+    new_x_min = min(one_x_min, two_x_min)
+    new_x_max = max(one_x_max, two_x_max)
+    new_y_min = min(one_y_min, two_y_min)
+    new_y_max = max(one_y_max, two_y_max)
+
+    confidence_score = max(box_one[8], box_two[8])
+
+    return np.array([new_x_min, new_y_min, new_x_max, new_y_min, new_x_min, new_y_max, new_x_max, new_y_max, confidence_score])
+
+def merge_overlapping_boxes(boxes):
+    # the loop will reach its end once no overlap occurs 
+    i = 0
+    while i < len(boxes):
+        j = 0
+        while j < len(boxes):
+            print(i, j)
+            if i == j:
+                j += 1
+                continue
+            
+            if has_overlap(boxes[i], boxes[j]):
+                print("foudn overlap")
+                # set the first box to the merged box
+                boxes[i] = merge_boxes(boxes[i], boxes[j])
+
+                # delte the second box 
+                mask = np.ones(len(boxes), dtype=bool)
+                mask[j] = False
+                boxes = boxes[mask]
+
+                # reset to start fro mbeginning again
+                i = 0
+                j = 0
+                continue
+            j += 1
+        i += 1
+    
+    return boxes
+
 def draw_boxes(img, image_name, boxes, scale):
     # extract name of img file from full path 
     base_name = image_name.split('/')[-1]
 
     # open a text file to write bounding box coordinates 
     with open('data/results/' + 'res_{}.txt'.format(base_name.split('.')[0]), 'w') as f:
-        for box in boxes:
-            # filter out very small boxes 
-            if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3] - box[0]) < 5:
-                continue
+        # filter out very small boxes 
+        boxes = filter_boxes(boxes)
 
+        # merge overlapping boxes
+        boxes = merge_overlapping_boxes(boxes)
+
+        for box in boxes:
             # assign colors based on confidence score
             if box[8] >= 0.9:
                 color = (0, 255, 0)
@@ -51,10 +131,10 @@ def draw_boxes(img, image_name, boxes, scale):
                 color = (255, 0, 0)
             
             # draw box
-            cv2.line(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), color, 2)
-            cv2.line(img, (int(box[0]), int(box[1])), (int(box[4]), int(box[5])), color, 2)
-            cv2.line(img, (int(box[6]), int(box[7])), (int(box[2]), int(box[3])), color, 2)
-            cv2.line(img, (int(box[4]), int(box[5])), (int(box[6]), int(box[7])), color, 2)
+            cv2.line(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), color, 1)
+            cv2.line(img, (int(box[0]), int(box[1])), (int(box[4]), int(box[5])), color, 1)
+            cv2.line(img, (int(box[6]), int(box[7])), (int(box[2]), int(box[3])), color, 1)
+            cv2.line(img, (int(box[4]), int(box[5])), (int(box[6]), int(box[7])), color, 1)
 
             # calculate coords based on original image scale to revert to original image dimesions 
             min_x = min(int(box[0] / scale), int(box[2] / scale), int(box[4] / scale), int(box[6] / scale))
